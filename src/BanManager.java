9c51d703fc90afa0d45490d8e1f8fa47cf0f2d4f
Michael Camp <mcam320@wgu.edu>
2026-02-01 15:34:27 -0600

    adds final variable TEAM_SIZE to feed size to previously-added arrays with explicit intent

--- src/BanManager.java
+++ src/BanManager.java
@@ -1,10 +1,11 @@
-public class BanManager {
-    private Player[] playerList1 = new Player[5];
-    private Player[] playerList2 = new Player[5];
-    private Pick[] picks1 = new Pick[5];
-    private Pick[] picks2 = new Pick[5];
+public class BanManager { //class meant to group players together and determine bans
+    private static final int TEAM_SIZE = 5;
+    private Player[] playerList1 = new Player[TEAM_SIZE];
+    private Player[] playerList2 = new Player[TEAM_SIZE];
+    private Pick[] picks1 = new Pick[TEAM_SIZE];
+    private Pick[] picks2 = new Pick[TEAM_SIZE];
 
     public BanManager() {
-    
+        
     }
 }
