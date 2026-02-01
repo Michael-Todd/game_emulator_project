36f9af99ffcf0f9b93717f5329efcc87ea8a1855
Michael Camp <mcam320@wgu.edu>
2026-02-01 15:11:46 -0600

    adds core variables to BanManager

--- src/BanManager.java
+++ src/BanManager.java
@@ -1,3 +1,9 @@
 public class BanManager {
+    private Player[] playerList = new Player[10];
+    private Pick[] picksOne = new Pick[5];
+    private Pick[] picksTwo = new Pick[5];
+
+    public BanManager() {
     
+    }
 }
