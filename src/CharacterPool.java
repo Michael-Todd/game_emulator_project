9f516c165e8565b3f7ff50d323be547347e59aff
Michael Camp <mcam320@wgu.edu>
2026-02-01 15:06:17 -0600

    adds more characters to CHARACTERS

--- src/CharacterPool.java
+++ src/CharacterPool.java
@@ -4,11 +4,13 @@ public class CharacterPool {
     
     private static final String[] CHARACTERS = {
         "char1", "char2", "char3", "char4",
-        "char5", "char6", "char7", "char8"
+        "char5", "char6", "char7", "char8",
+        "char9", "char10", "char11", "char12"
     };
 
     private Random rand = new Random();
 
+    //chosen to be nonstatic in case future me wishes to work with multiple character pools
     public Character getCharacter() {
 
         String name = CHARACTERS[rand.nextInt(CHARACTERS.length)];
