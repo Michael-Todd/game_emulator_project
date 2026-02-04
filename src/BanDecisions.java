e984b4d8087515da2afceef3c34e5bdf3e1bd667
Michael Camp <mcam320@wgu.edu>
2026-02-04 15:32:10 -0600

    adds helper functions, cleans up constructor

--- src/BanDecisions.java
+++ src/BanDecisions.java
@@ -1,105 +1,70 @@
 import java.util.*;
 
-public class BanDecisions {
+public class BanDecisions {//responsible for using Pick[] teams accessed from TeamBuilder object to determine which Characters are banned and displaying said bans
     private HashMap<Character, Integer> teamVotes1 = new HashMap<>(); //banned Character, number of votes for team1
     private HashMap<Character, Integer> teamVotes2 = new HashMap<>(); //banned Character, number of votes for team2
     private Integer max;
     private Character maxCharacter;
-    private Set<Character> bannedCharacters = new LinkedHashSet<>(); //changed from ArrayList to prevent duplicates
-
+    private Set<Character> bannedCharacters = new LinkedHashSet<>(); //chosen over ArrayList to prevent duplicates
 
     
     public BanDecisions(Pick[] team1, Pick[] team2) {
 
-        for (int i=0; i<team1.length; i++) {
-            if (teamVotes1.containsKey(team1[i].character())) {
-                teamVotes1.put(team1[i].character(), teamVotes1.get(team1[i].character())+1);
-            }
-            else {
-                teamVotes1.put(team1[i].character(), 1);
-            }
-        }
+        tallyTeamVotes(team1, teamVotes1);
+        tallyTeamVotes(team2, teamVotes2);
 
-        for (int i=0; i<team2.length; i++) {
-            if (teamVotes2.containsKey(team2[i].character())) {
-                teamVotes2.put(team2[i].character(), teamVotes2.get(team2[i].character())+1);
-            }
-            else {
-                teamVotes2.put(team2[i].character(), 1);
-            }
-        }
-
-        max = 0;
-        maxCharacter = null;
-
-        for (Map.Entry<Character, Integer> entry: teamVotes1.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            if (value > max) {
-                maxCharacter = key;
-                max = value;
-            }
-        }
-
-        bannedCharacters.add(maxCharacter);
-        teamVotes1.remove(maxCharacter);
-
-        max = 0;
-        maxCharacter = null;
-
-        for (Map.Entry<Character, Integer> entry: teamVotes1.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            if (value > max) {
-                maxCharacter = key;
-                max = value;
-            }
-        }
-
-        if (maxCharacter != null) { //in the event that teamVotes1 consists of all the same keys, the second loop would never iterate, and maxCharacter would be null
-            bannedCharacters.add(maxCharacter);
-            teamVotes1.remove(maxCharacter); //in case I decide to ban more in the future, it makes sense to remove again
-        }
-
-
-        max = 0;
-        maxCharacter = null;
-
-        for (Map.Entry<Character, Integer> entry: teamVotes2.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            if (value > max) {
-                maxCharacter = key;
-                max = value;
-            }
-        }
-
-        bannedCharacters.add(maxCharacter);
-        teamVotes2.remove(maxCharacter);
-
-        max = 0;
-        maxCharacter = null;
-
-        for (Map.Entry<Character, Integer> entry: teamVotes2.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            if (value > max) {
-                maxCharacter = key;
-                max = value;
-            }
-        }
-
-        if (maxCharacter != null) { //in the event that teamVotes1 consists of all the same keys, the second loop would never iterate, and maxCharacter would be null
-            bannedCharacters.add(maxCharacter);
-            teamVotes2.remove(maxCharacter); //in case I decide to ban more in the future, it makes sense to remove again
-        }
+        findTeamBans(teamVotes1);
+        findTeamBans(teamVotes2);
 
     }
+
+
+    private void tallyTeamVotes(Pick[] team, HashMap<Character, Integer> teamVotes) {
+        for (int i=0; i<team.length; i++) {
+            if (teamVotes.containsKey(team[i].character())) {
+                teamVotes.put(team[i].character(), teamVotes.get(team[i].character())+1);
+            }
+            else {
+                teamVotes.put(team[i].character(), 1);
+            }
+        }
+    }
+
+    private void findTeamBans(HashMap<Character, Integer> teamVotes) {
+        max = 0;
+        maxCharacter = null;
+        for (Map.Entry<Character, Integer> entry: teamVotes.entrySet()) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value > max) {
+                maxCharacter = key;
+                max = value;
+            }
+        }
+        
+        bannedCharacters.add(maxCharacter);
+        teamVotes.remove(maxCharacter);
+
+        max = 0;
+        maxCharacter = null;
+        for (Map.Entry<Character, Integer> entry: teamVotes.entrySet()) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value > max) {
+                maxCharacter = key;
+                max = value;
+            }
+        }
+        if (maxCharacter!= null) {
+            bannedCharacters.add(maxCharacter);
+            teamVotes.remove(maxCharacter);
+        }
+    }
+
+
     public void printBannedCharacters(){
         for (Character bannedCharacter: bannedCharacters) {
             System.out.println(bannedCharacter.getName() + " is banned.");
         }
     }
 }
-
-//TODO: wrap repeated code blocks in constructor into private helper functions for readability's sake
