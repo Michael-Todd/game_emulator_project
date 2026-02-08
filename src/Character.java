1261bd31fd6c5d36261518ad58299dfa15e5ba89
Michael Camp <mcam320@wgu.edu>
2026-02-07 18:41:45 -0600

    adds JavaDoc comments

--- src/Character.java
+++ src/Character.java
@@ -1,11 +1,26 @@
-public class Character {//creation of Characters lives solely in CharacterPool, where a fixed set of 12 Characters are made/stored
+/**
+ * Represents a character in the videogame.
+ * Each Character has a name.
+ * *Note that there is a static list of Characters, which is stored and handled by the class CharacterPool
+ */
+public class Character {
     private String name;
 
+    /**
+     * Constructs a Character object with its name.
+     * 
+     * @param name the name of this Character object
+     */
     public Character(String name) {
 
         this.name = name;
     }
     
+    /**
+     * Returns the name of this Character object.
+     * 
+     * @return the Character's name
+     */
     public String getName() {
         
         return name;
