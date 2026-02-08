052b0fbe6404ee180c91c64c7389e879987b5184
Michael Camp <mcam320@wgu.edu>
2026-02-08 11:43:22 -0600

    adds JavaDoc comments

--- src/Pick.java
+++ src/Pick.java
@@ -1,1 +1,4 @@
-public record Pick(Character character, int pickOrder) {}
+/**
+ * Designed to hold a Player's bannedCharacter and pickOrder, utilized in TeamBuilder and BanDecisions.
+ */
+public record Pick(Character character, int pickOrder) {} 
