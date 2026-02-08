7519ebe4437db43d88b32ddceabd879d6e4789f4
Michael Camp <mcam320@wgu.edu>
2026-02-07 18:25:50 -0600

    adds JavaDoc comments

--- src/Player.java
+++ src/Player.java
@@ -1,27 +1,49 @@
+/**
+ * Represents a player in the videogame.
+ * Each Player has a pick order and votes for a Character to ban.
+ */
 public class Player {
     private int pickOrder;
     private Character bannedCharacter;
 
+    
+
+    /**
+     * Constructs a Player object with a given pick order.
+     * 
+     * @param pickOrder the order in which this Player object is loaded into team
+     */
     public Player(int pickOrder) {
 
         this.pickOrder = pickOrder;
     }
 
-
+    /**
+     * Randomly selects a Character object using CharacterPool.
+     * 
+     * @return the Character object that this Player votes to ban
+     */
     public Character banCharacter() {
-
 
         this.bannedCharacter = CharacterPool.getCharacter();
         return bannedCharacter;
     }
 
-    
+    /**
+     * Returns the pick order for this Player.
+     * 
+     * @return the Player's pick order
+     */
     public int getPickOrder() {
 
         return pickOrder;
     }
 
-
+    /**
+     * Returns the Character that this Player voted to ban.
+     * 
+     * @return the Player's ban vote
+     */
     public Character getBannedCharacter() {
 
         return bannedCharacter;
