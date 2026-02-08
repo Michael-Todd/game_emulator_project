b82c0d2792e7dfaafb4f2095e3377bf8a7670c29
Michael Camp <mcam320@wgu.edu>
2026-02-08 11:59:27 -0600

    adds JavaDoc comments

--- src/TeamBuilder.java
+++ src/TeamBuilder.java
@@ -1,4 +1,8 @@
-public class TeamBuilder { //class "makes players", groups players together and has them select their bannedCharacter vote -- a team returned from an object here is a Pick consiting of the Character bannedCharacter each Player wishes to ban and that Player's pickorder
+/**
+ * Represents the building of the two competing player teams in the videogame.
+ * TeamBuilder creates Player objects, groups them into teams, and ultimately grants access to a Pick array representing each team.
+ */
+public class TeamBuilder {
     private static final int TEAM_SIZE = 5;
     
     private Player[] playerList1 = new Player[TEAM_SIZE];
@@ -6,10 +10,11 @@ public class TeamBuilder { //class "makes players", groups players together and 
     private Pick[] picks1 = new Pick[TEAM_SIZE];
     private Pick[] picks2 = new Pick[TEAM_SIZE];
 
-
+    /**
+     * Constructs a TeamBuilder object, each of which creates two random teams.
+     * Each object is bound to be distinct from any other in terms of the contents of the Pick arrays representing each team.
+     */
     public TeamBuilder() {
-
-        System.out.println("TeamBuilder running.."); //TODO: remove when done testing
 
         makeTeam();
 
@@ -17,7 +22,9 @@ public class TeamBuilder { //class "makes players", groups players together and 
         populatePicks(playerList2, picks2);
     }
 
-    
+    /**
+     * Contains the logic to construct an array of Player objects, the Players that make up a team.
+     */
     private void makeTeam() {
         for (int i=0; i<TEAM_SIZE; i++) {
             playerList1[i] = new Player(i+1);
@@ -25,17 +32,32 @@ public class TeamBuilder { //class "makes players", groups players together and 
         }
     }
 
+    /**
+     * Contains the logic to construct an array of Pick type, representing the relevant data of a team.
+     * 
+     * @param playerList an array of Player objects as constructed by makeTeam
+     * @param picks the Pick array to be populated
+     */
     private void populatePicks(Player[] playerList, Pick[] picks) {
         for (int i=0; i<TEAM_SIZE; i++) {
             picks[i] = new Pick(playerList[i].banCharacter(), playerList[i].getPickOrder());
         }
-    } //to fill picks1 and picks2 using the Players from playerList1 and playerList2 and the instance method banCharacter
-
+    }
+    
+    /**
+     * the Pick array representing the first team
+     * 
+     * @return the first team (relevant data: bannedCharacter vote, pickOrder)
+     */
     public Pick[] getTeam1() {
         return picks1;
     }
 
-
+    /**
+     * the Pick array representing the second team
+     * 
+     * @return the second team (relevant data: bannedCharacter vote, pickOrder)
+     */
     public Pick[] getTeam2() {
         return picks2;
     }
