94982409eaff45bb866e4e4ba829411f6477c070
Michael Camp <mcam320@wgu.edu>
2026-02-08 12:30:49 -0600

    Fills main function with logic to execute program

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hey, Java!!");
+        TeamBuilder teamBuilder = new TeamBuilder();
+        Pick[] team1 = teamBuilder.getTeam1();
+        Pick[] team2 = teamBuilder.getTeam2();
+
+        BanDecisions banDecisions = new BanDecisions(team1, team2);
+        banDecisions.printBannedCharacters();
     }
 }
