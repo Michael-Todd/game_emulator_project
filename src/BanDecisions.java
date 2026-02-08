c2d539b9651388d110e0e27b43e8d2edae13a267
Michael Camp <mcam320@wgu.edu>
2026-02-08 12:10:29 -0600

    adds JavaDoc comments

--- src/BanDecisions.java
+++ src/BanDecisions.java
@@ -1,11 +1,21 @@
 import java.util.*;
 
-public class BanDecisions {//responsible for using Pick[] teams accessed from TeamBuilder object to determine which Characters are banned and displaying said bans
+/**
+ * Represents tha process of making the ban decisions based on the two team's choices.
+ * Responsible for using the Pick teams accessed from a TeamBuilder object to determined which Characters are banned for the match.
+ * Displays these Characters.
+ */
+public class BanDecisions {
     private HashMap<Character, Integer> teamVotes1 = new HashMap<>(); //banned Character, number of votes for team1
     private HashMap<Character, Integer> teamVotes2 = new HashMap<>(); //banned Character, number of votes for team2
     private Set<Character> bannedCharacters = new LinkedHashSet<>(); //chosen over ArrayList to prevent duplicates
 
-    
+    /**
+     * Constructs a BanDecisions object given two teams.
+     * 
+     * @param team1 the first team as returned by a TeamBuilder object
+     * @param team2 the second team as returned by the same TeamBuilder object
+     */
     public BanDecisions(Pick[] team1, Pick[] team2) {
 
         tallyTeamVotes(team1, teamVotes1);
@@ -16,7 +26,12 @@ public class BanDecisions {//responsible for using Pick[] teams accessed from Te
 
     }
 
-
+    /**
+     * Tracks the number of votes from a team for each voted Character.
+     * 
+     * @param team a team as returned by a TeamBuilder object
+     * @param teamVotes a HashMap to keep track of the number of votes casted for each Character
+     */
     private void tallyTeamVotes(Pick[] team, HashMap<Character, Integer> teamVotes) {
         for (int i=0; i<team.length; i++) {
             if (teamVotes.containsKey(team[i].character())) {
@@ -28,23 +43,25 @@ public class BanDecisions {//responsible for using Pick[] teams accessed from Te
         }
     }
 
+    /**
+     * Determines the Characters that are banned.
+     * 
+     * @param teamVotes the HashMap of ban votes as returned from tallyTeamVotes
+     */
     private void findTeamBans(HashMap<Character, Integer> teamVotes) {
+        banMaxCharacter(teamVotes);
+        banMaxCharacter(teamVotes);
+    }
+
+    /**
+     * Determines one of the Characters that are banned.
+     * 
+     * @param teamVotes the HashMap of ban votes as returned from talleyTeamVotes
+     */
+    private void banMaxCharacter(HashMap<Character, Integer> teamVotes) {
         Integer max = 0;
         Character maxCharacter = null;
-        for (Map.Entry<Character, Integer> entry: teamVotes.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            if (value > max) {
-                maxCharacter = key;
-                max = value;
-            }
-        }
-        
-        bannedCharacters.add(maxCharacter);
-        teamVotes.remove(maxCharacter);
 
-        max = 0;
-        maxCharacter = null;
         for (Map.Entry<Character, Integer> entry: teamVotes.entrySet()) {
             Character key = entry.getKey();
             Integer value = entry.getValue();
@@ -53,13 +70,16 @@ public class BanDecisions {//responsible for using Pick[] teams accessed from Te
                 max = value;
             }
         }
-        if (maxCharacter!= null) {
+
+        if (maxCharacter != null) {// this is a guard in the case that all Players picked the same Character to ban, and is only really needed for the second call
             bannedCharacters.add(maxCharacter);
             teamVotes.remove(maxCharacter);
         }
     }
 
-
+    /**
+     * Prints the Characters that are banned.
+     */
     public void printBannedCharacters(){
         for (Character bannedCharacter: bannedCharacters) {
             System.out.println(bannedCharacter.getName() + " is banned.");
