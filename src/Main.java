fca8cd366f5d3d6895769bef0d9852c48369e8aa
Michael Camp <mcam320@wgu.edu>
2026-02-08 23:01:11 -0600

    adds print display of all the ban votes from each team

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,17 @@ public class Main {
         Pick[] team1 = teamBuilder.getTeam1();
         Pick[] team2 = teamBuilder.getTeam2();
 
+        for (Pick pick: team1) {
+            System.out.println("From the first team, ban pick " + pick.pickOrder() + " is " + pick.character().getName());
+        }
+
+        System.out.println();
+
+        for (Pick pick: team2) {
+            System.out.println("From the second team, ban pick " + pick.pickOrder() + " is " + pick.character().getName());
+        }
+
+        System.out.println("--------------------------------------------");
         BanDecisions banDecisions = new BanDecisions(team1, team2);
         banDecisions.printBannedCharacters();
     }
